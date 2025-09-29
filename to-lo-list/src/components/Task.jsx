6010add846ef183a2783d6a64051e908beecd804
lawriss02 <blaukook@gmail.com>
2025-09-29 17:26:05 +0200

    Task component styling

--- to-lo-list/src/components/Task.jsx
+++ to-lo-list/src/components/Task.jsx
@@ -11,16 +11,16 @@ export function Task({task, onClick }) {
     return (
         <div className="task" onClick={onClick}>
             <div className="task-content-left">
-                <div className="task-parameters-icons">
+                <div className="task-parameters-icons-t">
                     <img className="task-icon-priority" src={task.selectedPriority} alt=""/>
                     {task.selectedCategory && (<label htmlFor="">{task.selectedCategory}</label>) }
                 </div>
                 <p>{task.title}</p>
             </div>
             
-            <div className="task-bar">
-                <div className="task-bar-out" style={{backgroundColor: task.selectedPending.color}}>
-                    <div className="task-bar-in" style={{height: task.selectedPending.height}}>
+            <div className="task-bar-t">
+                <div className="task-bar-out-t" style={{backgroundColor: task.selectedPending.color}}>
+                    <div className="task-bar-in-t" style={{height: task.selectedPending.height}}>
                     </div>
                 </div>
             </div>
